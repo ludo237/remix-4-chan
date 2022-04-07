f22ce8cacec2aee1b9653981a32844c54f03f932
ludo237 <ludovico1990@hotmail.it>
2022-04-07 09:52:14 +0200

    Add basic style to work on and finish up the initial setup
    Signed-off-by: ludo237 <ludovico1990@hotmail.it>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,26 +7,37 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./compiled.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix 4Chan",
+  description: "4Chan if it was built with Remix",
+  keywords: "4chan, remix, clone",
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "shortcut icon", href: "https://s.4cdn.org/image/favicon.ico" },
+    { rel: "apple touch icon", href: "https://s.4cdn.org/image/apple-touch-icon-iphone.png" },
+  ];
+};
+
+export default function App () {
   return (
     <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+    <head>
+      <Meta />
+      <Links />
+    </head>
+    <body className={"min-h-screen bg-gradient-to-b from-orange-200 via-amber-50 to-yellow-50"}>
+    <Outlet />
+    <ScrollRestoration />
+    <Scripts />
+    <LiveReload />
+    </body>
     </html>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,21 @@
-export default function Index() {
+export default function Index () {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className={"max-w-lg mx-auto pt-10"}>
+      {/* Card design */}
+      <div>
+        <div className={"flex justify-between p-1 border border-b-0 border-black bg-rose-700 text-white"}>
+          <div className={"flex-1 font-bold text-lg"}>What is 4Chan?</div>
+          <div>Header Options</div>
+        </div>
+        <div className={"border border-t-0 border-black p-1 bg-white"}>
+          <p>
+            4chan is a simple image-based bulletin board where anyone can post comments and share images. There are boards dedicated to a variety of
+            topics, from Japanese animation and culture to videogames, music, and photography. Users do not need to register an account before
+            participating in the community. Feel free to click on a board below that interests you and jump right in! Be sure to familiarize
+            yourself with the Rules before posting, and read the FAQ if you wish to learn more about how to use the site.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
